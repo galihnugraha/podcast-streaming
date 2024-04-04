c367049d0f839e39bf73bb75521dd2f6230ef0cf
Galih Nugraha <galihnugraha58@gmail.com>
2024-04-04 08:48:43 +0700

    revisi search podcast by genre and title

--- src/main/kotlin/com/proyekakhir/podcaststreaming/repository/PodcastRepository.kt
+++ src/main/kotlin/com/proyekakhir/podcaststreaming/repository/PodcastRepository.kt
@@ -4,11 +4,13 @@ import com.proyekakhir.podcaststreaming.domain.entity.GenreEntity
 import com.proyekakhir.podcaststreaming.domain.entity.PodcastEntity
 import com.proyekakhir.podcaststreaming.domain.entity.TypeEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface PodcastRepository: JpaRepository<PodcastEntity, Int> {
-    fun findByTitle(title:String?): List<PodcastEntity>
+    fun findAllByIdGenre(genre: GenreEntity): List<PodcastEntity>
 
     fun findAllByIdType(type: TypeEntity): List<PodcastEntity>
 
-    fun findAllByIdGenre(genre: GenreEntity): List<PodcastEntity>
+    @Query("SELECT p FROM PodcastEntity p WHERE p.title LIKE %?1% OR p.idGenre.genre LIKE %?1%")
+    fun findByGenreAndTitle(s: String): List<PodcastEntity>
 }
--- src/main/kotlin/com/proyekakhir/podcaststreaming/service/impl/PodcastServiceImpl.kt
+++ src/main/kotlin/com/proyekakhir/podcaststreaming/service/impl/PodcastServiceImpl.kt
@@ -156,12 +156,14 @@ class PodcastServiceImpl(
     override fun search(token: String, s: String): ResMessageDto<List<ResPodcastDto>> {
         val claim = JwtGenerator().decodeJwt(token)
 
-        val genreName = genreRepository.findByGenre(s)
+//        val genreName = genreRepository.findByGenre(s)
+//
+//        val podcasts = if (genreName != null)
+//            podcastRepository.findAllByIdGenre(genreName)
+//        else
+//            podcastRepository.findByTitle(s)
 
-        val podcasts = if (genreName != null)
-            podcastRepository.findAllByIdGenre(genreName)
-        else
-            podcastRepository.findByTitle(s)
+        val podcasts = podcastRepository.findByGenreAndTitle(s)
 
         val response = arrayListOf<ResPodcastDto>()
         for (podcast in podcasts){
